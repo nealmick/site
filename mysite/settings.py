41e13e3057063e9091cb90a0a6d31c81791f11c9
Neal <33704976+nealmick@users.noreply.github.com>
2022-09-09 08:05:27 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ttt.apps.TttConfig',
     'crispy_forms',
     'predict.apps.PredictConfig',
     'users.apps.UsersConfig',####users
